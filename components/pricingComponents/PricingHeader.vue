0eabe3cfade19793a7ef9f25abe993159921da46
Elijah Elrod <elijah.elrod@yahoo.com>
2022-06-28 13:56:57 -0400

    Edited copy

--- components/pricingComponents/PricingHeader.vue
+++ components/pricingComponents/PricingHeader.vue
@@ -1,8 +1,8 @@
 <template>
    <header class="text-center mt-5">
-      <h3 class="text-5xl font-bold mb-5 text-gray-900">Our Pricing Plan</h3>
+      <h3 class="text-5xl font-bold mb-5 text-gray-900">Simple Pricing</h3>
       <p class="text-xl text-gray-400 mb-2">
-        Change or cancel plans any time.
+        We only get paid when you do.
       </p>
     </header>
 </template>
