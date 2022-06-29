9ab28efb4dcf79c0ea506e9281858c4e0f7308cd
e elrod <elijah.elrod@yahoo.com>
2022-06-29 14:36:29 -0400

    footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,17 @@
   <div>
     <Navbar />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 
 <script>
 import Navbar from "../components/landingComponents/Navbar.vue";
+import Footer from "../components/landingComponents/Footer.vue"
 
 export default {
-  components: { Navbar },
+  components: { Navbar, Footer },
 };
 </script>
 
