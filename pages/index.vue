fa0dfb282c3465d94db3e9d6ce4b067d71c333ec
javierecheverria <javier.ruben.echeverria@gmail.com>
2022-07-01 17:09:03 -0400

    added landing here

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+   
+    <Hero />
+    <Testimonials />
+    <Features />
+    <CTA />
+    
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '../components/landingComponents/Navbar.vue'
+import Hero from '../components/landingComponents/Hero.vue'
+import Features from '../components/landingComponents/Features.vue'
+import CTA from '../components/landingComponents/CTA.vue'
+import Testimonials from '../components/landingComponents/Testimonials.vue'
+
+
+
+export default Vue.extend({
+  components: { Navbar, Hero, Features, CTA, Testimonials },
+  name: 'IndexPage',
+
+})
+</script>
+
+<style>
+</style>
